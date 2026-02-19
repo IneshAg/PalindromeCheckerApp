011670ccb2fefa0cd4254790fa6f7712d1182525
IneshAg <ineshag123@gmail.com>
2026-02-19 10:09:54 +0530

    Base application setup

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+class PalindromeCheckerApp {
+
+    static final String APPLICATION_NAME = "PalindromeChecker App";
+    static final String APPLICATION_VERSION = "Version 1.0";
+
+
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println("Welcome to " + APPLICATION_NAME);
+        System.out.println(APPLICATION_VERSION);
+        System.out.println("=======================================");
+        System.out.println("This application checks whether a given string is a palindrome.");
+        System.out.println();
+
+        System.out.println("Application started successfully.");
+    }
+}
