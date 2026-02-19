bb9ac87358c6fc9131b615200ca46c7744f3d65e
IneshAg <Ineshag123@gmail.com>
2026-02-19 11:04:55 +0530

    UC4 - Character Array Based Palindrome Check

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class UseCase3PalindromeCheckerApp {
+class UseCase4PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
